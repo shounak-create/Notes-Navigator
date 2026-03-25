09bad63396e5b3e2db2ee49d30ee451edd816c1a
Shounak Pandit <68001165+shounak-create@users.noreply.github.com>
2026-03-25 19:43:41 +0530

    models have created

---
+++ server/models/Tasks.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const TasksSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    }
+},
+  {
+    timestamps: true,
+  })
+
+export default mongoose.model("Tasks",TasksSchema)
